d809983a0931b014a8d2110bd98fa79fe535e323
AdeleGropplero <adele.gdt@hotmail.com>
2025-02-06 15:00:52 +0100

    Start day2

--- src/test/java/com/epicode/projects/ProjectsApplicationTests.java
+++ src/test/java/com/epicode/projects/ProjectsApplicationTests.java
@@ -1,17 +1,27 @@
 package com.epicode.projects;
 
+import com.epicode.projects.Esercizio.D2.Ordine;
+import com.epicode.projects.Esercizio.D2.Tavolo;
+import com.epicode.projects.Esercizio.Drink;
+import com.epicode.projects.Esercizio.Menu;
+import com.epicode.projects.Esercizio.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProjectsApplicationTests {
 	@Autowired
 	ProjectsApplication app;
+
+	@Autowired
+	private OrderService orderService;
+
+
 
 	@Test
 	void contextLoads() {
@@ -24,8 +34,32 @@ class ProjectsApplicationTests {
 
 	@Test
 	void myTest(){
-		assert true;
+		//assert true;
 
 	}
+
+	@Test
+	void limonataPrezzo(){
+		Menu menu = orderService.createMenu();
+		Drink limonata = menu.getDrinks().stream().filter(elem -> elem.getNome().contains("Limonata")).findFirst().orElse(null);
+		double result = limonata.getPrezzo();
+		assertEquals(1.29, result);
+	}
+
+	@Test
+	void contoCorretto(){
+		Ordine ordine = orderService.createOrdine1();
+		assertTrue(ordine.getContoTotale() > 20.0);
+	}
+
+	@Test
+	void tavoloEsistente(){
+		Tavolo tavolo = orderService.createTable();
+		assertNotNull(tavolo);
+	}
+
+
+
+
 
 }
